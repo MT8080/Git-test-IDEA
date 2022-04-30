99830ba1f709ba86016d2dd4b1d6489c71d58796
MT8080 <MT8080@qq.com>
2022-05-01 02:05:26 +0800

    master test commit

--- src/main/java/com/mt/Git/GitTest.java
+++ src/main/java/com/mt/Git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("Hello Git22被修改变成蓝色!!");
         System.out.println("hello Word!!!!!!");
         System.out.println("hello Word!!!!!!");
+
+        System.out.println("当前分支master test");
     }
 }
