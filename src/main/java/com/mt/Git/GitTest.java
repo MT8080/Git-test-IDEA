56c9255db1361ff9ec7bbb58935f30900285a063
MT8080 <MT8080@qq.com>
2022-05-01 02:04:14 +0800

    second commit

--- src/main/java/com/mt/Git/GitTest.java
+++ src/main/java/com/mt/Git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
         System.out.println("Hello Git22被修改变成蓝色!!");
+        System.out.println("hello Word!!!!!!");
+        System.out.println("hello Word!!!!!!");
     }
 }
