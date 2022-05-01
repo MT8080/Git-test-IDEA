3f099b60c0f467151df31464b53145e4985867a8
MT8080 <MT8080@qq.com>
2022-05-01 19:02:49 +0800

    gitee commit test

--- src/main/java/com/mt/Git/GitTest.java
+++ src/main/java/com/mt/Git/GitTest.java
@@ -15,5 +15,9 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push test");
         System.out.println("push test");
+
+        System.out.println("gitee test");
+        System.out.println("gitee test");
+
     }
 }
